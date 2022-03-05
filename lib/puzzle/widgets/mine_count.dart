755f4f6a8d4e19af0d63eb1c643b0a4814f07b93
Yashawant Retharekar <ygretharekar@outlook.com>
2022-03-06 03:45:58 +0530

    add mine count animation

--- lib/puzzle/widgets/mine_count.dart
+++ lib/puzzle/widgets/mine_count.dart
@@ -1,4 +1,5 @@
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mineswiper/puzzle/layout/responsible_layout_builder.dart';
 import 'package:mineswiper/puzzle/providers/puzzle_pro.dart';
@@ -10,46 +11,85 @@ class MineCount extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final mines = ref.watch(mineCountProvider);
+    final animationController = useAnimationController(
+      duration: Duration(
+        milliseconds: 300,
+      ),
+    )..repeat();
+    final curve = CurvedAnimation(
+      parent: animationController,
+      curve: Curves.decelerate,
+    );
+
+    final _animOffsetDown =
+        Tween<Offset>(begin: const Offset(0, 100), end: Offset.zero)
+            .animate(curve);
+    final _animOffsetUp =
+        Tween<Offset>(begin: Offset.zero, end: const Offset(0, -100))
+            .animate(curve);
+
+    Future.delayed(Duration.zero, () {
+      // animationController.reset();
+      animationController.forward();
+    });
+
+    final pre = usePrevious(mines);
 
     Widget getWidget(double dim) {
       return Padding(
         padding: const EdgeInsets.all(8.0),
         child: mines == 0
             ? const SizedBox()
-            : Stack(
-                children: [
-                  const Positioned.fill(child: FlutterLogo()),
-                  Align(
-                    alignment: Alignment.center,
-                    child: SizedBox(
-                      child: Acrylic(
-                        child: Center(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              children: [
-                                Text(
-                                  'M',
-                                  style: FluentTheme.of(context)
-                                      .typography
-                                      .display,
-                                  textAlign: TextAlign.center,
-                                ),
-                                Text(
-                                  '$mines',
-                                  style: FluentTheme.of(context)
-                                      .typography
-                                      .titleLarge,
-                                  textAlign: TextAlign.center,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
+            : AnimatedBuilder(
+                animation: animationController,
+                builder: (context, child) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'M: ',
+                        style: FluentTheme.of(context).typography.display,
+                        textAlign: TextAlign.center,
                       ),
-                    ),
-                  ),
-                ],
+                      // if (!_animOffsetUp.isCompleted)
+                      //   Transform.translate(
+                      //     offset: _animOffsetUp.value,
+                      //     child: Text(
+                      //       '${mines - 1}',
+                      //       style:
+                      //           FluentTheme.of(context).typography.titleLarge,
+                      //       textAlign: TextAlign.center,
+                      //     ),
+                      //   ),
+                      Stack(
+                        children: [
+                          if (pre != 0)
+                            Transform.translate(
+                              offset: _animOffsetUp.value,
+                              child: Text(
+                                '${mines + 1}',
+                                style: FluentTheme.of(context)
+                                    .typography
+                                    .titleLarge,
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          if (mines >= 0)
+                            Transform.translate(
+                              offset: _animOffsetDown.value,
+                              child: Text(
+                                '$mines',
+                                style: FluentTheme.of(context)
+                                    .typography
+                                    .titleLarge,
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                        ],
+                      ),
+                    ],
+                  );
+                },
               ),
       );
     }
